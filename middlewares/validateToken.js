763cbc75f5b5cd6930dc6bd5300d36b67a2c484d
ManuelHMR <manuelhmrjf@gmail.com>
2022-06-16 20:43:50 -0300

    feat: validate token

--- middlewares/validateToken.js
+++ middlewares/validateToken.js
@@ -1,3 +1,12 @@
 export function validateToken(req, res, next){
-
+    const { authorization } = req.headers;
+    const token = authorization?.replace('Bearer ', '').trim(); 
+    const key = process.env.TOKEN_KEY;
+    jwt.verify(token, key, (err, result) => { 
+        if(err) return res.status(401).send({ err: err });
+        if(result) {
+            res.locals.userId = result;
+            next();
+        }
+    });
 };
