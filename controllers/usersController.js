5c54292d1977e583ef4bfa80a92d6fba62c90ca6
Leonardo Lucas <leolucasguedes@yahoo.com.br>
2022-06-18 15:10:02 -0300

    fix: validadeToken tags/usersRouters

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -7,7 +7,7 @@ export async function getUserPosts(req, res) {
     if (result.rowCount === 0) {
       return res.sendStatus(404);
     }
-    res.status(200).send(result);
+    res.status(200).send(result.rows);
   } catch (e) {
     console.error(e);
     res.status(500).send("Erro de conexão com servidor");
--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { getUserPosts } from "../controllers/usersController.js";
+import { validateToken } from "../middlewares/validateToken.js";
 
 const usersRouter = Router();
 
-usersRouter.get("/user/:id", getUserPosts);
+usersRouter.get("/user/:id", validateToken, getUserPosts);
 
 export default usersRouter;
