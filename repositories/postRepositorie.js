792286c901dd828f97eeebe2499719204b8f46c7
Manuel Henrique Martins Rodrigues <manuelhmrjf@gmail.com>
2022-06-18 19:12:08 -0300

    Update postRepositorie.js

--- repositories/postRepositorie.js
+++ repositories/postRepositorie.js
@@ -3,7 +3,7 @@ import connection from "../config/database.js";
 export async function regiterLike(postId, userId) {
   return connection.query(
     `
-        INSERT INTO likes (postId, userId)
+        INSERT INTO likes ("postId", "userId")
         VALUES ($1, $2)
     `,
     [postId, userId]
@@ -14,7 +14,7 @@ export async function removeLike(postId, userId) {
   return connection.query(
     `
         DELETE FROM likes
-        WHERE postId = $1 AND userId = $2
+        WHERE "postId" = $1 AND "userId" = $2
     `,
     [postId, userId]
   );
